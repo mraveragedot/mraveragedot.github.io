598740a87486717d459e0edb3cc77b5755182e2a
mraveragedot <nohlgustafson05@gmail.com>
2023-10-09 14:16:06 -0600

    made targets fly across the screen
    this was way harder then it should've been

--- 02.assignment/sketch.js
+++ 02.assignment/sketch.js
@@ -5,6 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let targetdelay = 1000;
+let lastTarget = 0
+let targets = []
+time = 30
+let target;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +18,45 @@ function setup() {
 
 function draw() {
   background(220);
+  makeTargets();
+  moveTargets();
+}
+
+function makeTargets(){
+  if (millis() > lastTarget + targetdelay){
+    target = {
+      x: random(0,50.001),
+      y: height/12,
+      speed: random(5,10),
+      diameter: random(35,40),
+      colour: ["blue", "red", "yellow"],
+    };
+    console.log(target.x)
+    if (target.x <= 25){
+      target.x = 0 - target.diameter/2;
+    }
+    else{
+      target.x = width + target.diameter/2;
+      target.speed = target.speed * -1;
+      target.y += target.diameter + 5
+    }
+    targets.push(target);
+    lastTarget = millis();
+    time -= 1;
+  }
+}
+
+function moveTargets(){
+  if (targets.length != 0){
+    for(i = 0; i < targets.length - 1; i ++){
+      fill(targets[i].colour[0]);
+      circle(targets[i].x, targets[i].y, targets[i].diameter);
+      fill(targets[i].colour[1]);
+      circle(targets[i].x, targets[i].y, targets[i].diameter * (2/3));
+      fill(targets[i].colour[2]);
+      circle(targets[i].x, targets[i].y, targets[i].diameter/3);
+      targets[i].x += targets[i].speed
+    }
+  }
+
 }
