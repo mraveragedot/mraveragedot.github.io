415d551e1c203d14f7c05f9c71f60148d0e337a3
mraveragedot <nohlgustafson05@gmail.com>
2023-10-10 12:17:16 -0600

    made a line
    ...

--- 02.assignment/sketch.js
+++ 02.assignment/sketch.js
@@ -6,9 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 let targetdelay = 1000;
-let lastTarget = 0
-let targets = []
-time = 30
+let lastTarget = 0;
+let targets = [];
+let time = 30;
 let target;
 
 
@@ -20,6 +20,7 @@ function draw() {
   background(220);
   makeTargets();
   moveTargets();
+  shoot();
 }
 
 function makeTargets(){
@@ -38,7 +39,7 @@ function makeTargets(){
     else{
       target.x = width + target.diameter/2;
       target.speed = target.speed * -1;
-      target.y += target.diameter + 5
+      target.y += target.diameter + 5;
     }
     targets.push(target);
     lastTarget = millis();
@@ -47,16 +48,20 @@ function makeTargets(){
 }
 
 function moveTargets(){
-  if (targets.length != 0){
-    for(i = 0; i < targets.length - 1; i ++){
+  if (targets.length !== 0){
+    for(let i = 0; i < targets.length - 1; i ++){
       fill(targets[i].colour[0]);
       circle(targets[i].x, targets[i].y, targets[i].diameter);
       fill(targets[i].colour[1]);
       circle(targets[i].x, targets[i].y, targets[i].diameter * (2/3));
       fill(targets[i].colour[2]);
       circle(targets[i].x, targets[i].y, targets[i].diameter/3);
-      targets[i].x += targets[i].speed
+      targets[i].x += targets[i].speed;
     }
   }
 
+}
+
+function shoot(){
+  line(width/2, height, mouseX, mouseY);
 }
