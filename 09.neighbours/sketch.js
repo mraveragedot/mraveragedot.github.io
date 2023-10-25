93a4f8c2041c9a1b75cf49aa17e91d5dca13fe4b
mraveragedot <nohlgustafson05@gmail.com>
2023-10-25 12:14:46 -0600

    neighborss

--- 09.neighbours/sketch.js
+++ 09.neighbours/sketch.js
@@ -1,66 +1,95 @@
-// 2d grid neighbours
+// 2D Array Grid
+// Dan Schellenberg
+// Oct 24, 2023
 
+// let grid = [[1, 0, 0, 1],
+//             [0, 0, 1, 1],
+//             [1, 1, 0, 1],
+//             [0, 1, 1, 1]];
+
+let grid;
 let cellSize;
-let grid = [];
-
-const GRID_SIZE = 10;
+const GRID_SIZE = 15;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  if (width/GRID_SIZE < height/GRID_SIZE){
+
+  if (height > width) {
     cellSize = width/GRID_SIZE;
   }
-  else{
+  else {
     cellSize = height/GRID_SIZE;
   }
-  grid = createGrid(GRID_SIZE, GRID_SIZE);
+  grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
 }
 
 function draw() {
   background(220);
-  displayGrid(GRID_SIZE, GRID_SIZE);
+  displayGrid();
 }
 
-function createGrid(cols, rows){
-  let newGrid = [];
-  for(let y = 0; y < rows; y++){
-    grid.push([]);
-    for(let x = 0; x < cols; x ++){
-      let number = random(0,100);
-      if (number < 50){
-        grid[x].push(1);
-      }
-      else{
-        grid[x].push(0);
-      }
+function mousePressed(){
+  let y = Math.floor(mouseY/cellSize);
+  let x = Math.floor(mouseX/cellSize);
+
+  toggleCell(x, y); // current cell
+  toggleCell(x, y-1); // up a cell
+  toggleCell(x, y+1); // down a cell
+  toggleCell(x-1, y); // left a cell
+  toggleCell(x+1, y); // right a cell
+}
+
+
+function toggleCell(x,y){
+  if(x >= 0 && x <= GRID_SIZE - 1 && y >= 0 && y <= GRID_SIZE - 1){
+    if (grid [y][x] === 0){
+      grid[y][x] = 1;
+    }
+    else if (grid[y][x] === 1){
+      grid[y][x] = 0;
     }
   }
-  return newGrid;
 }
 
-function generateEmptyGrid(cols, rows){
-  let newGrid = [];
-  for(let y = 0; y < rows; y++){
-    grid.push([]);
-    for(let x = 0; x < cols; x ++){ 
-      grid[x].push(0);
-    }
-  }
-  return newGrid;
-
-}
-
-function displayGrid(){
-  for(let y = 0; y < GRID_SIZE; y ++){
-    for (let x = 0; x < GRID_SIZE; x ++){
-      if(grid[y][x] === 0){
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 0) {
         fill("white");
       }
-      else if (grid[y][x] === 1){
+      if (grid[y][x] === 1) {
         fill("black");
       }
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
   }
+}
+
+
+function generateRandomGrid(cols, rows) {
+  let randomArray = [];
+  for (let y = 0; y < cols; y++) {
+    randomArray.push([]);
+    for (let x = 0; x < rows; x++) {
+      if (random(100) < 50) {
+        randomArray[y].push(0);
+      }
+      else {
+        randomArray[y].push(1);
+      }
+    }
+  }
+  return randomArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let randomArray = [];
+  for (let y = 0; y < cols; y++) {
+    randomArray.push([]);
+    for (let x = 0; x < rows; x++) {
+      randomArray[y].push(0);
+    }
+  }
+  return randomArray;
 }
 
